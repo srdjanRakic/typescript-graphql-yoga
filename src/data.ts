9819a9f02785b26df7c5a61b35d807e85f44d0a6
srdjanRakic <srgjan.rakic@gmail.com>
2019-12-12 08:16:38 +0100

    Add more mock tasks

--- src/data.ts
+++ src/data.ts
@@ -13,6 +13,7 @@ export interface TaskData {
 export const projects: ProjectData[] = [
   { id: 1, name: "Learn React Native" },
   { id: 2, name: "Workout" },
+  { id: 3, name: "Buy Groceries" }
 ];
 
 export const tasks: TaskData[] = [
@@ -25,5 +26,8 @@ export const tasks: TaskData[] = [
   },
   { id: 3, title: "Install Xcode", completed: false, projectId: 1 },
   { id: 4, title: "Morning Jog", completed: true, projectId: 2 },
-  { id: 5, title: "Visit the gym", completed: false, projectId: 2 },
+  { id: 5, title: "Hit the gym", completed: false, projectId: 2 },
+  { id: 3, title: "Buy egges", completed: false, projectId: 3 },
+  { id: 3, title: "Buy chicken", completed: true, projectId: 3 },
+  { id: 3, title: "Buy BBQ sauce", completed: false, projectId: 3 },
 ];
